7326bbcc53d0bed56fea2a02593c2c1080472966
jyothiskariyilroy-arch <jyothiskariyilroy@gmail.com>
2026-04-29 13:13:04 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,7 +9,10 @@ const nextConfig = {
         pathname: '/storage/v1/object/public/**',
       },
     ],
-    formats: ['image/webp'],
+    formats: ['image/avif', 'image/webp'],
+    minimumCacheTTL: 86400,       // cache optimised images for 24h
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    imageSizes: [16, 32, 64, 96, 128, 256, 384],
   },
 };
 module.exports = nextConfig;
